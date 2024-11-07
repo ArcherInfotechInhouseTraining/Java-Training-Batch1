c3e132abc0694a3433902d40fd3311c0df3abf74
PallaviJadhav <pallavijadhav2213@gmail.com>
2024-11-07 17:17:05 +0530

    advance java

---
+++ Pallavi/Advance_Java/practicle/createServlet_2/src/main/java/arc/nov/examples/SecondServlet.java
@@ -0,0 +1,22 @@
+package arc.nov.examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SecondServlet extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		arg1.setContentType("text/html");
+		PrintWriter out = arg1.getWriter();
+		out.print("Hello From Generic Servlet...!!");
+		
+	}
+
+}
